48c53d69c106da8c01a17f0d20e3d6495868bc45
quocdat-le-insacvl <71183203+quocdat-le-insacvl@users.noreply.github.com>
2021-07-17 13:58:41 +0200

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -1,6 +1,10 @@
 sudo apt install vim git cmake curl
+sudo apt install build-essential cmake vim-nox python3-dev
+sudo apt install mono-complete golang nodejs default-jdk npm
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 git clone https://github.com/quocdat-le-insacvl/mydotfiles  
 cp mydotfiles/.vimrc ~/.vimrc
 vim ~/.vimrc
+
+cd ~/.vim/plugged/YouCompleteMe/
